654c01e163d4387c68ba31ca267a07ab12229f94
Jana Ismail <janaismail@janas-air.lan>
2024-07-08 11:08:02 -0500

    Rendered JSX function | Implemented useState() hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useState } from 'react'
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+
+  const [count, setCount] = useState(0) //useState() returns an array
+  
+  const handleBtnClick = () => {
+    setCount(count + 1)
+    console.log(count)
+  }
+
+  return (
+    <>
+      <h1>Welcome to React!</h1>
+      <div>useState() is dope!</div>
+      <button className="btn-primary" onClick={handleBtnClick}>
+        Click me!
+      </button>
+      <div>Count: {count}</div>
+    </>
+  )
 }
