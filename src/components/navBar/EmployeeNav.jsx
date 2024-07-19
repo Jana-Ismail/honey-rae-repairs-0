2ab9072dece386df8a2035c669a1534dcad8549f
Jana Ismail <janaismail@janas-air.lan>
2024-07-19 09:20:34 -0500

    Created CustomerView Component | Created Customer Tickets Link in Navbar

--- src/components/navBar/EmployeeNav.jsx
+++ src/components/navBar/EmployeeNav.jsx
@@ -2,7 +2,7 @@ import "./NavBar.css"
 import { Link, useNavigate } from "react-router-dom"
 import "./NavBar.css"
 
-export const NavBar = () => {
+export const EmployeeNav = () => {
     const navigate = useNavigate()
 
     return (
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,13 +1,6 @@
-import { Route, Routes, Outlet } from "react-router-dom"
-import { NavBar } from "../components/navBar/NavBar"
-import { Welcome } from "../components/welcome/Welcome"
-import { EmployeeDetails } from "../components/employees/EmployeeDetails.jsx"
-import { EmployeeList } from "../components/employees/EmployeeList.jsx"
-import { TicketList } from "../components/tickets/TicketList.jsx"
-import { CustomerList } from "../components/customers/CustomerList.jsx"
-import { CustomerDetails } from "../components/customers/CustomerDetails.jsx"
 import { useState, useEffect } from "react"
-import { EmployeeForm } from "../components/forms/EmployeeForm.jsx"
+import { EmployeeViews } from "./EmployeeViews"
+import { CustomerViews } from "./CustomerViews"
 
 export const ApplicationViews = () => {
   const [ currentUser, setCurrentUser ] = useState({})
@@ -19,28 +12,9 @@ export const ApplicationViews = () => {
     setCurrentUser(honeyUserObject)
   }, [])
   
-  return (
-    <Routes>
-      <Route 
-        path='/' 
-        element={
-          <>
-            <NavBar />
-            <Outlet />
-          </>
-        }>
-          <Route index element={<Welcome />} />
-          <Route path="tickets" element={<TicketList currentUser={currentUser} />} />
-          <Route path="employees">
-            <Route index element={<EmployeeList />} />
-            <Route path=":employeeId" element={<EmployeeDetails />} />
-          </Route>
-          <Route path="customers">
-            <Route index element={<CustomerList />} />
-            <Route path=":customerId" element={<CustomerDetails />} />
-          </Route>
-          <Route path="profile" element={<EmployeeForm currentUser={currentUser} />} />
-        </Route>
-    </Routes>
-  )
+  return currentUser.isStaff ? (
+  <EmployeeViews currentUser={currentUser} />
+) : (
+  <CustomerViews currentUser={currentUser} />
+)
 }
